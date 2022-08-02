ba5bea26375b24b949937219fac095e278c845b7
KWojcik243 <63014344+KWojcik243@users.noreply.github.com>
2022-08-02 19:39:09 +0200

    PostgreSQL DB and models

--- CryCom/models.py
+++ CryCom/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class CryptoValue(models.Model):
+class Crypto_Value(models.Model):
     full_name = models.CharField(max_length=20)
     code = models.CharField(max_length=10)
     price = models.FloatField()
+
+
+class Blog_Post(models.Model):  # Model under development
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateField()
+    blog_content = models.CharField(max_length=400)
+
+
+class Crypto_Group(models.Model):
+    name = models.CharField(max_length=20)
+    crypto_type = models.BooleanField() # 0 = If there have to be only one type of currency in the groups, 1 - in other cases
+    password = models.CharField(max_length=20)
+
+
+class Group_Members(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    crypto_group = models.ForeignKey(Crypto_Group, on_delete=models.CASCADE)
+
+
+class Team_Coins(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    buy_value = models.FloatField()
+    quantity = models.FloatField()
+    actual_value = models.FloatField()
+    highest_profit = models.FloatField()
+    crypto_group = models.ForeignKey(Crypto_Group, on_delete=models.CASCADE)
+
