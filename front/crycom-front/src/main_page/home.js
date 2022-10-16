25308fd0d3db2c443744156d97d8ada95c52fa1f
KWojcik243 <63014344+KWojcik243@users.noreply.github.com>
2022-10-17 00:51:56 +0200

    Add div for settings banner

--- front/crycom-front/src/main_page/home.js
+++ front/crycom-front/src/main_page/home.js
@@ -1,14 +1,29 @@
+// import ReactDOM from "react-dom"
 // import Nav from "../components/nav.js"
 function Page(){
-    return (
-        <div className="navbar">
-            <img className="logo"src="btc-logo.png"></img>
-            <button className="nav-button">HOME</button>
-            <button className="nav-button">ROOMS</button>
-            <button className="nav-button">BLOG</button>
+    let settins_status=true
+    function settingsShow(e){
 
-            <img className="user"src="user.jpg"></img>
+        console.log(settins_status);
+        settins_status = !settins_status
+    }
+    return (
+        <div>
+             <div className="navbar">
+                <img className="logo"src="btc-logo.png"></img>
+                <button className="nav-button">HOME</button>
+                <button className="nav-button">ROOMS</button>
+                <button className="nav-button">BLOG</button>
+                <img className="user"src="user.jpg" onClick={settingsShow}></img>
+            </div> 
+            <div className="options-box">
+                <p>Settings</p>
+                <p>Log out</p>
+            </div>
+            
         </div>
+       
+        
     )
 }
 
