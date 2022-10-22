7058e60a99ad63bb70d7bf40410f8cb67bde028c
KWojcik243 <63014344+KWojcik243@users.noreply.github.com>
2022-10-22 15:47:33 +0200

    ADD: method that returns list od supported crypto from coingecko

--- CryCom/Data/crypto_data.py
+++ CryCom/Data/crypto_data.py
@@ -1,11 +1,10 @@
 from pycoingecko import CoinGeckoAPI
-
-
-###TODOO
-#Index of Data must be date
+from pandas import DataFrame
+from datetime import datetime
 class CryptoData:
     cg = CoinGeckoAPI()
-    def get_historical_data(self, crypto_id:str, currency:str = 'usd', days:int = 1) -> list:
+
+    def get_historical_data(self, crypto_id:str, currency:str = 'usd', days:int = 1) -> DataFrame:
         """Method for getting historical data from CoinGecko
 
         Args:
@@ -16,12 +15,22 @@ class CryptoData:
                                             above 90 days from query time = daily data (00:00 UTC). Defaults to 1.
 
         Returns:
-            list: List that containts two-element lists with format [time,data] (time in milliseconds)
+            DataFrame: Dataframe that have as index date and column value
         """
-        return self.cg.get_coin_market_chart_by_id(id=crypto_id, vs_currency=currency, days=days)
+        data = list(self.cg.get_coin_market_chart_by_id(id=crypto_id, vs_currency=currency, days=days).values())[0]
+        date, value = [], []
+        for item in data:
+            date.append(datetime.fromtimestamp(item[0]/1000))
+            value.append(round(item[1], 2))
+
+        df = DataFrame()
+        df.index = date
+        df['value'] = value
+
+        return df
 
     def get_actual_data(self, crypto_ids:list or str, currency:str = 'usd') -> dict:
-        """_summary_
+        """Method for getting actual data from CoinGecko
 
         Args:
             crypto_ids (list or str): Name/names or id/ids of coin/coins from CoinGecko.
@@ -31,3 +40,15 @@ class CryptoData:
             dict: Dictionary that returns {Coin name:{currency, value}}
         """
         return self.cg.get_price(ids=crypto_ids, vs_currencies=currency)
+    
+    def get_supported_list_coins_ids(self) -> list:
+        """Method for getting list of ids supported by CoinGecko
+
+        Returns:
+            list: list of ids
+        """
+        data = self.cg.get_coins_markets(vs_currency='usd')
+        ids = []
+        for item in data:
+            ids.append(item['id'])
+        return ids
