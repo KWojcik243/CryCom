7df9fc7e88277fd2b38db13cfa7c841c3c86a6e7
KWojcik243 <KWojcik243@users.noreply.github.com>
2022-10-25 12:53:25 +0200

    Fix: add navbar settings buttons

--- front/src/components/nav.js
+++ front/src/components/nav.js
@@ -1,12 +1,11 @@
 import {useNavigate} from 'react-router-dom'
 import logo from '../main_page/btc-logo.png'
 import user from '../main_page/user.jpg'
-import '../main_page/nav.css';
+import './nav.css';
 export default function Nav(){
     let settins_status=true;
     const navigate = useNavigate()
     function settingsShow(e){
-        console.log(settins_status);
         settins_status = !settins_status
         {settins_status ? document.getElementById("op-box").style.display = "none" :
                           document.getElementById("op-box").style.display = "block"};
@@ -16,7 +15,7 @@ export default function Nav(){
              <div className="navbar">
                 <img className="logo"src={logo} alt='logo'></img>
                 <button className="nav-button" onClick={()=>{
-                    navigate('/login')
+                    navigate('/')
                 }}>HOME</button>
                 <button className="nav-button" onClick={()=>{
                     navigate('/rooms')
@@ -25,8 +24,10 @@ export default function Nav(){
                 <img className="user"src={user} alt='menu-logo' onClick={settingsShow}></img>
             </div>
             <div className="options-box" id="op-box">
-                <p>Settings</p>
-                <p>Log out</p>
+                <div className="p-wrapp"> <p className="one-option">Settings</p> </div>
+                <div className="p-wrapp"> <p className="one-option">Contact</p> </div>
+                <div className="p-wrapp"> <p className="one-option">Privacy</p> </div>
+                <div className="p-wrapp"> <p className="one-option">Log out</p> </div>
             </div>
         </div>
     )
--- front/src/rooms/rooms.js
+++ front/src/rooms/rooms.js
@@ -1,9 +1,14 @@
+import {useNavigate} from 'react-router-dom'
 import './rooms.css';
 import Brak from './brak_zdj.jpg'
 export default function Rooms(){
+    const navigate = useNavigate()
+    function goToRoom(e){
+        navigate('/rooms/1')
+    }
     return(
         <div className="main-box">
-            <div className="single-room">
+            <div className="single-room" onClick={goToRoom}>
                 <img className='room-image' src={Brak} />
                 <p className="room-name">Nazwa Pokoju</p>
             </div>
